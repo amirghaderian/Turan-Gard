c10818ca388dda24f5825a05a10aacc10249d651
soheil1405 <108219944+farzadkhani1@users.noreply.github.com>
2023-07-04 14:55:30 +0330

    add rounded card

--- src/pages/About/Cards.jsx
+++ src/pages/About/Cards.jsx
@@ -4,144 +4,68 @@ const Cards = () => {
   return (
     <>
       <div className="flex cards mt-16  ">
-        <div className="card1 border-2 flex-grow m-4">
+        <div className="card1 border-2 flex-grow m-4 rounded-full">
           <div className="icon h-24 text-center"><Key fontSize={'large'} className=' mt-8'/></div>
           <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
           
-          <p className="text-center p-5 pt-6 w-64"> ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
+          <p className="text-center p-5 pt-6 w-80 mx-auto"> ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
           </p>
           
         </div>
-        <div className="card2 border-2 flex-grow  m-4">
-          <div className="icon h-24 text-center"><SupportAgent fontSize={'large'} className=' mt-8'/></div>
+        <div className="card2 border-2 flex-grow m-4 rounded-full">
+          <div className="icon h-24 text-center "><SupportAgent fontSize={'large'} className=' mt-8'/></div>
           <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6">
+          <p className="text-center p-5 pt-6  w-80 mx-auto">
           ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
           </p>
         </div>
-        <div className="card3 border-2 flex-grow  m-4 ">
+        <div className="card3 border-2 flex-grow  m-4 rounded-full">
           <div className="icon h-24 text-center "><Diversity3 fontSize={'large'} className=' mt-8'/></div>
           <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6">
+          <p className="text-center p-5 pt-6 w-80 mx-auto">
           ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
           </p>
         </div>
-        <div className="card4 border-2 flex-grow  m-4">
+        <div className="card4 border-2 flex-grow  m-4 rounded-full">
           <div className="icon h-24 text-center  "><TipsAndUpdates fontSize={'large'} className=' mt-8'/></div>
           <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6">
+          <p className="text-center p-5 pt-6 w-80 mx-auto">
           ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
           </p>
         </div>
       </div>
       <div className="flex cards mt-16  ">
-        <div className="card1 border-2 flex-grow m-4">
+        <div className="card1 border-2 flex-grow m-4 rounded-full">
           <div className="icon h-24 text-center">icon</div>
           <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6">
+          <p className="text-center p-5 pt-6 w-80 mx-auto">
             ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
             پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
             می‌کنیم.
           </p>
         </div>
-        <div className="card2 border-2 flex-grow  m-4">
+        <div className="card2 border-2 flex-grow  m-4 rounded-full">
           <div className="icon h-24 text-center">icon</div>
           <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6">
+          <p className="text-center p-5 pt-6 w-80 mx-auto">
             ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
             پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
             می‌کنیم.
           </p>
         </div>
-        <div className="card3 border-2 flex-grow  m-4 ">
+        <div className="card3 border-2 flex-grow  m-4 rounded-full">
           <div className="icon h-24 text-center ">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
+          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
+          <p className="text-center p-5 pt-6 w-80 mx-auto">
             ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
             پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
             می‌کنیم.
           </p>
         </div>
-        <div className="card4 border-2 flex-grow  m-4">
+        <div className="card4 border-2 flex-grow  m-4 rounded-full">
           <div className="icon h-24 text-center  ">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-      </div>
-      <div className="flex cards mt-16  ">
-        <div className="card1 border-2 flex-grow m-4">
-          <div className="icon h-24 text-center">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card2 border-2 flex-grow  m-4">
-          <div className="icon h-24 text-center">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card3 border-2 flex-grow  m-4 ">
-          <div className="icon h-24 text-center ">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card4 border-2 flex-grow  m-4">
-          <div className="icon h-24 text-center  ">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-      </div>
-      <div className="flex cards mt-16  ">
-        <div className="card1 border-2 flex-grow m-4">
-          <div className="icon h-24 text-center">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card2 border-2 flex-grow  m-4">
-          <div className="icon h-24 text-center">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card3 border-2 flex-grow  m-4 ">
-          <div className="icon h-24 text-center ">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
-            ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card4 border-2 flex-grow  m-4">
-          <div className="icon h-24 text-center  ">icon</div>
-          <h2 className="text-center">گفت خنثی و شنود موثر</h2>
-          <p className="text-center">
+          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3">گفت خنثی و شنود موثر</h2>
+          <p className="text-center p-5 pt-6 w-80 mx-auto">
             ما در علی‌بابا گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
             پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
             می‌کنیم.
