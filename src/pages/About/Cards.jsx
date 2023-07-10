50ccc28ca19a338a372b857633af7d333bab1513
soheil1405 <108219944+farzadkhani1@users.noreply.github.com>
2023-07-10 12:46:53 +0330

    responsiv about component

--- src/pages/About/Cards.jsx
+++ src/pages/About/Cards.jsx
@@ -1,78 +1,109 @@
-import React from 'react'
-import { Diversity3, Key, SupportAgent, TipsAndUpdates } from '@mui/icons-material';
+import React from "react";
+import {
+  Diversity3,
+  Key,
+  SupportAgent,
+  TipsAndUpdates,
+} from "@mui/icons-material";
 const Cards = () => {
   return (
-    <>
-      <div className="flex cards mt-16">
-        <div className="card1 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
-          <div className="icon h-24 text-center"><Key  sx={{ fontSize: 60 , color: '#243c5a'}}    className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          
-          <p className="text-center p-5 pt-6 w-80 mx-auto"> ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
-          </p>
-          
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+      <div className="card1 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <Key className="mt-8 text-[#243c5a] text-4xl" />
         </div>
-        <div className="card2 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
-          <div className="icon h-24 text-center "><SupportAgent sx={{ fontSize: 60 , color: '#243c5a'}}  className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6  w-80 mx-auto">
-          ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
-          </p>
-        </div>
-        <div className="card3 border-2 flex-grow  m-4 rounded-full border-[#243c5a]">
-          <div className="icon h-24 text-center "><Diversity3 sx={{ fontSize: 60 , color: '#243c5a'}}  className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6 w-80 mx-auto">
-          ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
-          </p>
-        </div>
-        <div className="card4 border-2 flex-grow  m-4 rounded-full border-[#243c5a]">
-          <div className="icon h-24 text-center  "><TipsAndUpdates sx={{ fontSize: 60 , color: '#243c5a'}} className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6 w-80 mx-auto">
-          ما در توران گرد همیشه به دنبال راه حل های جذاب تر و کوتاه تر برای افزایش بازدهی و کیفیت خدماتمان هستیم
-          </p>
-        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
       </div>
-      <div className="flex cards mt-16  ">
-        <div className="card1 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
-          <div className="icon h-24 text-center"><TipsAndUpdates sx={{ fontSize: 60 , color: '#243c5a'}} className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6 w-80 mx-auto">
-            ما در توران گرد گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
+      <div className="card2 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <SupportAgent className="mt-8 text-[#243c5a] text-4xl" />
         </div>
-        <div className="card2 border-2 flex-grow  m-4 rounded-full border-[#243c5a]">
-          <div className="icon h-24 text-center"><TipsAndUpdates sx={{ fontSize: 60 , color: '#243c5a'}} className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6 w-80 mx-auto">
-            ما در توران گرد گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card3 border-2 flex-grow  m-4 rounded-full border-[#243c5a]">
-          <div className="icon h-24 text-center "><TipsAndUpdates sx={{ fontSize: 60 , color: '#243c5a'}} className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6 w-80 mx-auto">
-            ما در توران گرد گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
-        <div className="card4 border-2 flex-grow  m-4 rounded-full border-[#243c5a] ">
-          <div className="icon h-24 text-center  "><TipsAndUpdates sx={{ fontSize: 60 , color: '#243c5a'}} className=' mt-8'/></div>
-          <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3" style={{color:"#243c5a",fontWeight:600,fontSize:"18px"}}>گفت خنثی و شنود موثر</h2>
-          <p className="text-center p-5 pt-6 w-80 mx-auto">
-            ما در توران گرد گفته‌های همکاران را بدون پیش‌زمینه می‌شنویم و اهل
-            پیش‌داوری نیستم. ضمن اینکه همیشه منظورمان را واضح و بدون تفسیر بیان
-            می‌کنیم.
-          </p>
-        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
       </div>
-    </>
+      <div className="card3 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <Diversity3 className="mt-8 text-[#243c5a] text-4xl" />
+        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
+      </div>
+      <div className="card4 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <TipsAndUpdates className="mt-8 text-[#243c5a] text-4xl" />
+        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
+      </div>
+      <div className="card4 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <TipsAndUpdates className="mt-8 text-[#243c5a] text-4xl" />
+        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
+      </div>
+      <div className="card4 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <TipsAndUpdates className="mt-8 text-[#243c5a] text-4xl" />
+        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
+      </div>
+      <div className="card4 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <TipsAndUpdates className="mt-8 text-[#243c5a] text-4xl" />
+        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
+      </div>
+      <div className="card4 border-2 flex-grow m-4 rounded-full border-[#243c5a]">
+        <div className="icon h-24 text-center">
+          <TipsAndUpdates className="mt-8 text-[#243c5a] text-4xl" />
+        </div>
+        <h2 className="text-center underline underline-offset-20 decoration-dashed decoration-3 text-[#243c5a] font-semibold text-lg">
+          گفت خنثی و شنود موثر
+        </h2>
+        <p className="text-center p-5 pt-6 w-80 mx-auto">
+          ما در توران گرد همیشه به دنبال راه حل‌های جذاب‌تر و کوتاه‌تر برای
+          افزایش بازدهی و کیفیت خدماتمان هستیم
+        </p>
+      </div>    </div>
   );
 };
 
